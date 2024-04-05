27052abf122552b7e0e4b78d5ec5ae82aa974110
Sergey <graff.MS@rambler.ru>
2024-04-05 10:37:27 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[]names = new String[4];
+        names[0] = "James Hetfield";
+        names[1] = "Lars Ulrich";
+        names[2] = "Kirk Hammett";
+        names[3] = "Jason Newsted";
+        System.out.println("Состав группы Metallica (1986-2001):  "
+                + names[0] + ", " + names[1] + ", "
+                + names[2] + ", " + names[3]);
     }
 }
