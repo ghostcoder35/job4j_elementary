50bfcd881ec5394e7791202f402300b9d5041907
Sergey <graff.MS@rambler.ru>
2024-04-07 08:46:43 +0300

    6.1. Классический поиск перебором. [#33489 #490130]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -4,8 +4,8 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         for (int i = 1; i < data.length; i++) {
-            result = data[0] == data[i];
-            if (!result) {
+            if (data[0] != data[i]) {
+                result = false;
                 break;
             }
         }
