f5acaa970df998e098035c66038d13c959a014e8
Sergey <graff.MS@rambler.ru>
2024-04-17 10:34:50 +0300

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,10 +1,10 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    public static final String NEW_VALUE = "";
+    private static final String NEW_VALUE = "";
     private int sizeOfEmpty = 10;
-    public String surname;
-    String name;
+    private String surname;
+    private String name;
 
     Broken() {
     }
